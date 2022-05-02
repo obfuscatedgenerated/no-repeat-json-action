b362f343df4faf2e042c25bbdc14f2242c116d83
obfuscatedgenerated <75035364+obfuscatedgenerated@users.noreply.github.com>
2022-05-02 17:44:24 +0100

    actually working!

--- action.js
+++ action.js
@@ -1,5 +1,8 @@
 const core = require("@actions/core");
 
+const clarinet = require("clarinet");
+const parser = clarinet.parser();
+
 const fs = require("fs");
 const path = require("path");
 
@@ -10,20 +13,18 @@ var duplicates = {};
 
 var numDupes = 0;
 
+var currentKeys = [];
+
+parser.onkey = parser.onopenobject = k => {
+    currentKeys.push(k);
+};
+
 for (let i in files) {
     let currentPath = path.join(workspace, files[i]);
     let currentContents = fs.readFileSync(currentPath, "utf8");
-    let currentJSON = JSON.parse(currentContents);
-    let currentKeys = Object.keys(currentJSON);
-    duplicates[files[i]].push(
-        currentKeys.filter(item => {
-            if (set.has(item)) {
-                set.delete(item);
-            } else {
-                return item;
-            }
-        })
-    );
+    currentKeys = [];
+    parser.write(currentContents).close();
+    duplicates[files[i]] = currentKeys.filter((e, index, arr) => arr.indexOf(e) !== index);
     if (duplicates[files[i]].length > 0) {
         numDupes++;
     }
@@ -31,5 +32,7 @@ for (let i in files) {
 
 if (numDupes > 0) {
     core.setOutput("duplicates", JSON.stringify(duplicates));
-    core.setFailed(`${numDupes} duplicate keys found in JSON files. Keys duplicated by file: ${duplicates}`);
+    core.setFailed(`${numDupes} duplicate keys found in JSON files. Keys duplicated by file: ${JSON.stringify(duplicates)}`);
+} else {
+    core.info("All files passed validation!");
 }
